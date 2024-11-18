d8c038c2b5477f65737d4b816d3d99904de5a1f9
Amadou Oumarou <amadougoge1@gmail.com>
2024-11-18 19:29:41 +0100

    added network correction 2

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -1,17 +1,13 @@
 # Network Infrastructure
-
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block           = "10.1.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 }
 
-
 # Internet Gateway needed for Inbound access to ALB
-
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
-
   tags = {
     Name = "${local.prefix}-main"
   }
